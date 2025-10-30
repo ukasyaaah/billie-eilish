fad379ab676641f5b4dbe5bd7c03f71e6350a7b5
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-10-30 18:12:16 +0700

    initial

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="flex bg-black flex-col selected-style justify-center items-center mx-auto min-h-screen"
+  >
+    <div
+      className="flex selected-style font-wwaf flex-col mx-auto test-font  justify-end text-3xl md:text-5xl  lg:text-7xl text-white"
+    >
+      <p>WHEN WE ALL FALL ASLEEP,</p>
+      <span>WHERE DO WE GO?</span>
+    </div>
+  </div>
+</template>
