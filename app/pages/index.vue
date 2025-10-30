a79fb65a351d5849324e8e275e476ecf63c58360
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-10-30 20:25:52 +0700

    fix: index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,13 +2,8 @@
 
 <template>
   <div
-    class="flex bg-black text-white font-wwaf bg-bottom bg-cover flex-col selected-style justify-center items-center  lg:mx-auto min-h-screen"
+    class="flex flex-col text-left bg-black text-3xl md:text-5xl lg:text-7xl text-white font-wwaf bg-bottom bg-cover selected-style justify-center items-center lg:mx-auto min-h-screen"
   >
-    <div
-      className="flex selected-style flex-col mx-auto justify-end text-3xl md:text-5xl lg:text-7xl "
-    >
-      <p>WHEN WE ALL FALL ASLEEP,</p>
-      <span>WHERE DO WE GO?</span>
-    </div>
+    <p class="flex flex-col text-left  items-left">WHEN WE ALL FALL ASLEEP, <span>WHERE DO WE GO?</span></p>
   </div>
 </template>
