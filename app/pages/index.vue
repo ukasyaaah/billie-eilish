d65d39fd1028555b09c9a7bb6ef02fc1a1c5a42c
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-10-30 20:59:47 +0700

    add: github button

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,19 @@
 
 <template>
   <div
-    class="flex flex-col text-left bg-black text-3xl md:text-5xl lg:text-7xl text-white font-wwaf bg-bottom bg-cover selected-style justify-center items-center lg:mx-auto min-h-screen"
+    class="flex flex-col px-3 bg-black relative lg:flex-row lg:gap-x-10 h-screen text-2xl md:text-5xl lg:text-6xl text-white font-wwaf bg-bottom bg-cover selected-style justify-center items-center lg:mx-auto"
   >
-    <p class="flex flex-col text-left  items-left">WHEN WE ALL FALL ASLEEP, <span>WHERE DO WE GO?</span></p>
+    <UButton
+      class="absolute top-5 left-5"
+      to="https://github.com/ukasyaaah"
+      icon="i-lucide-github"
+      size="md"
+      variant="subtle"
+    />
+
+    <NuxtImg src="/when.png" alt="When" sizes="600" />
+    <p class="flex flex-col text-left items-left">
+      WHEN WE ALL FALL ASLEEP, <span>WHERE DO WE GO?</span>
+    </p>
   </div>
 </template>
