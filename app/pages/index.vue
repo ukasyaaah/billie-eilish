8c49721f601285b6994cd4ad34a63279bf3a3a08
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-10-30 19:38:12 +0700

    replace favicon

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,11 @@
-<script setup lang="ts">
-definePageMeta({
-  layoutTransition: true,
-  pageTransition: true,
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div
-    class="flex bg-black font-wwaf bg-bottom bg-cover flex-col selected-style justify-center items-center mx-auto min-h-screen"
+    class="flex bg-black text-white font-wwaf bg-bottom bg-cover flex-col selected-style justify-center items-center  lg:mx-auto min-h-screen"
   >
     <div
-      className="flex selected-style  flex-col mx-auto test-font  justify-end text-3xl md:text-5xl  lg:text-7xl text-white"
+      className="flex selected-style  flex-col mx-auto test-font  justify-end text-3xl md:text-5xl  lg:text-7xl "
     >
       <p>WHEN WE ALL FALL ASLEEP,</p>
       <span>WHERE DO WE GO?</span>
