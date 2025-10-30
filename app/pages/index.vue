103e9c70c631c431087b4fab39d178da076c6b2c
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-10-30 21:17:35 +0700

    fix image

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="flex flex-col px-3 bg-black relative lg:flex-row lg:gap-x-10 h-screen text-2xl md:text-5xl lg:text-6xl text-white font-wwaf bg-bottom bg-cover selected-style justify-center items-center lg:mx-auto"
+    class="flex flex-col px-3 bg-black relative lg:flex-row lg:gap-x-10 min-h-screen text-2xl md:text-5xl lg:text-6xl text-white font-wwaf bg-bottom bg-cover selected-style justify-center items-center lg:mx-auto"
   >
     <UButton
       class="absolute top-5 left-5"
@@ -12,7 +12,7 @@
       variant="subtle"
     />
 
-    <NuxtImg src="/when.png" alt="When" sizes="600" />
+    <NuxtImg src="/when.png" alt="When" sizes="100vw sm:50vw" />
     <p class="flex flex-col text-left items-left">
       WHEN WE ALL FALL ASLEEP, <span>WHERE DO WE GO?</span>
     </p>
